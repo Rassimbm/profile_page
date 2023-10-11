ad40a5d04afe4f74d37c38fb8f361b1e956cd4ce
Rassimbm <rassimbenmhamed@outlook.com>
2023-10-10 21:16:52 -0500

    completed first challenge: updated index and added code to script

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("page loaded ...");
+
+var userName = document.querySelector(".card-body h1")
+console.log(userName);
+
+
+function editName() {
+    userName.innerText = "Stacy A paterson";
+}
