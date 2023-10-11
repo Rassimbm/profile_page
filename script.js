e69ec8bd93e726c97422f61eee86b9e6a2bb566b
Rassimbm <rassimbenmhamed@outlook.com>
2023-10-10 21:36:45 -0500

    completed second challenge: updated index and added code to script

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 console.log("page loaded ...");
 
-var userName = document.querySelector(".card-body h1")
+var userName = document.querySelector(".card-body h1");
 console.log(userName);
-
 
 function editName() {
     userName.innerText = "Stacy A paterson";
+}
+
+var cnxRequest = document.querySelector(".user-line");
+console.log(cnxRequest);
+
+function accept() {
+    cnxRequest.remove()
+}
+
+function decline() {
+    cnxRequest.remove()
 }
